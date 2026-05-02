9684755db74d5a9fca55c7993c6b8b5791802370
ChrisNjagi <nyambarichris@gmail.com>
2026-05-02 15:44:51 +0300

    faet: implement student grading system and 2D array.

--- Student marks managment/Manag.java
+++ Student marks managment/Manag.java
@@ -12,18 +12,37 @@ public class Manag{
             System.out.println();
         }
 
-        int Total = 0;
-        for(int j=0; j<marks[0].length; j++){
-                Total += marks[0][j];
-        }
-        System.out.println("Student 1 marks: " + Total);
-//Second student marks
-        Total = 0;
-        for(int j=0; j<marks[1].length; j++){
-             Total += marks[1][j];
+//         int Total = 0;
+//         for(int j=0; j<marks[0].length; j++){
+//                 Total += marks[0][j];
+//         }
+//         System.out.println("Student 1 marks: " + Total);
+// //Second student marks
+//         Total = 0;
+//         for(int j=0; j<marks[1].length; j++){
+//              Total += marks[1][j];
              
-        }
-        System.out.println("Student 2 marks: " + Total);
+//         }
+//         System.out.println("Student 2 marks: " + Total);
+  int jo = marks.length;
+  System.out.println(jo);
 
+         for(int i=0; i<marks.length; i++){
+            int Total = 0;
+            for(int j=0; j<marks[i].length; j++){
+                Total += marks[i][j];
+            }
+            System.out.println("Student " + (i+1) + " marks: " + Total);
+            System.out.println("Student " + (i+1) + " average: " + (Total/marks[i].length));
+
+            int highest = marks[i][0];
+            for(int j=1; j<marks[i].length; j++){
+                if(marks[i][j] > highest){
+                    highest = marks[i][j];
+                }
+                
+            }
+            System.out.println("Student " + (i+1) + " highest mark: " + highest);
+        }
     }
 }
