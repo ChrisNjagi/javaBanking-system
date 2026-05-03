d68645ea36eb9198dbc54bc6b27f2968f7099d1f
ChrisNjagi <nyambarichris@gmail.com>
2026-05-03 08:38:06 +0300

    Adding logic of counting the no of days in the attendace

--- Student Attendance system/Stu.java
+++ Student Attendance system/Stu.java
@@ -9,6 +9,7 @@ public class Stu{
         //     {1, 1, 1, 0, 1},
         //     {0, 1, 0, 1, 1}
         // };
+       
         for(int i=0; i<Student.length; i++){
              System.out.println("Enter attendance for " + Student[i]);
 
@@ -17,13 +18,27 @@ public class Stu{
                 attendance[i][j] = input.nextInt();
                }
         }
+
+
         System.out.println("\nAttendance Record:");
         for(int i=0; i<Student.length; i++){
+            int count = 0;
             System.out.print(Student[i] + ": ");
             for(int j=0; j<attendance[i].length; j++){
-                System.out.print(attendance[i][j] );
+                if(attendance[i][j] == 1){
+                    ++count;
+                }
+              
             }
-            System.out.println();
+            System.out.println("Present: " + count + "/" + attendance[i].length + " days");
+             
         }
+        // int count = 0;
+        // for(int j=0; j<attendance[i].length; j++){
+        //     if(attendance[0][j] == 1){
+        //         count++;
+        //     }
+        // }
+        // System.out.println("Present: " + count);
     }   
 }
